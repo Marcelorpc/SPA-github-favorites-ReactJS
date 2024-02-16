0f2cfb0138966e8355cb0953a4bd73164c00af0b
marcelorpc <marcelorpchaves@gmail.com>
2024-02-16 13:04:57 -0300

    add profile structure

--- src/components/AppList.jsx
+++ src/components/AppList.jsx
@@ -1,6 +1,31 @@
+import { useState } from 'react'
 import styles from './AppList.module.css'
+import { UserProfile } from './UserProfile'
 
 export function AppList() {
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      avatar_ulr: 'https://avatars.githubusercontent.com/u/105247533?v=4',
+      name: 'Marcelo Paiva Chaves',
+      login: 'Marcelorpc',
+      public_repos: 20,
+      followers: 18,
+    },
+    {
+      id: 2,
+      avatar_ulr: 'https://avatars.githubusercontent.com/u/105247533?v=4',
+      name: 'Marcelo Paiva Chaves2',
+      login: 'Marcelorpc2',
+      public_repos: 202,
+      followers: 182,
+    },
+  ])
+
+  function testeTeste(){
+    console.log('oi')
+  }
+
   return (
     <div className={styles.appList}>
       <div className={styles.listHeader}>
@@ -16,22 +41,20 @@ export function AppList() {
       </div>
 
       <div className={styles.usersList}>
-        <div className={styles.userSection}>
-          <div className={styles.userProfile}>
-            <img src="https://github.com/Marcelorpc.png" alt="Foto de perfil do usuario" />
-
-            <div className={styles.userProfileText}>
-              <strong>Marcelo Paiva</strong>
-              <span>marcelorpc</span>
-            </div>
-          </div>
-
-          <div className={styles.profileDetails}>
-            <p>25</p>
-            <p>84</p>
-            <button>Remover</button>
-          </div>
-        </div>
+        {
+          users.map(user => {
+            return (
+              <UserProfile 
+              id={user.id}
+              avatar_url={user.avatar_ulr}
+              name={user.name}
+              login={user.login}
+              public_repos={user.public_repos}
+              followers={user.followers}
+              />
+            )
+          })
+        }
       </div>
     </div>
   )
