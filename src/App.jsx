175d1be300ac940d4ef13decc7280a5c0432464d
marcelorpc <marcelorpchaves@gmail.com>
2024-02-16 11:51:12 -0300

    add app list

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './global.css'
 import styles from './App.module.css'
  
 import { AppHeader } from './components/AppHeader'
+import { AppList } from './components/AppList'
 
 function App() {
   return (
     <main>
       <div className={styles.app}>
         <AppHeader />
+        <AppList />
       </div>
     </main>
   )
